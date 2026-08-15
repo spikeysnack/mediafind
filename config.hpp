588b47b4abbdac5cf522966fc3daa0d88ee1d0b9
spikeysnack@gmail.com <spikeysnack@gmail.com>
2026-08-15 02:40:16 -0700

    MACROS for dirs and dbs

--- config.hpp
+++ config.hpp
@@ -8,7 +8,7 @@
 #ifndef BUILD_TEST
 // set to true after editing
 constexpr bool EDITED = false;
-// constexpr bool EDITED = true;
+//constexpr bool EDITED = true;
 
 constexpr std::string_view must_edit =
   "\n\nYOU MUST EDIT config.hpp "
@@ -16,59 +16,75 @@ constexpr std::string_view must_edit =
   "Then set EDITED to true.\n\n";
 
 static_assert(EDITED, must_edit);
+
+// base dir for db_files
+const string base_dir = getenv("HOME");
+#define BASE(D)  (base_dir + "/" + D)  
+
+// use your own dirs
+// set a dir to store database files (default: user home dir)
+
+#define VIDEO_DIR1 "/media/chris/xeon_movies1"
+#define VIDEO_DB1  BASE(".mediadb.xeon_movies1")
+
+#define VIDEO_DIR2  "/media/chris/xeon_movies2"
+#define VIDEO_DB2   BASE(".mediadb.xeon_movies2")
+
+#define VIDEO_DIR3  "/media/chris/xeon_movies3"
+#define VIDEO_DB3   BASE(".mediadb.xeon_movies3")
+
+#define VIDEO_DIR4  "/media/chris/TV"
+#define VIDEO_DB4   BASE(".mediadb.tv")          
+
+#define VIDEO_DIR5  "/media/chris/video_460G"
+#define VIDEO_DB5   BASE(".mediadb.video_460G")
+
+#define VIDEO_DIRS { VIDEO_DIR1, VIDEO_DIR2, VIDEO_DIR3, VIDEO_DIR4, VIDEO_DIR5 }
+#define VIDEO_DBS  { VIDEO_DB1 , VIDEO_DB2 , VIDEO_DB3 , VIDEO_DB4 , VIDEO_DB5  }
+
+
+#define AUDIO_DIR1 "/Audio"
+#define AUDIO_DB1  BASE(".mediadb.Audio")
+
+#define AUDIO_DIR2 "/Audio2"
+#define AUDIO_DB2  BASE(".mediadb.Audio2")
+
+#define AUDIO_DIR3 "/Audio3"
+#define AUDIO_DB3  BASE(".mediadb.Audio3")
+
+#define AUDIO_DIR4 "/Audio4"
+#define AUDIO_DB4 BASE(".mediadb.Audio4")
+
+#define AUDIO_DIRS { AUDIO_DIR1, AUDIO_DIR2, AUDIO_DIR3, AUDIO_DIR4 }
+#define AUDIO_DBS  { AUDIO_DB1 , AUDIO_DB2 , AUDIO_DB3 , AUDIO_DB4  }
+
 #endif
+
 
 #include "mediafind.hpp"
 
 /* this all happens befor main */
 
-// set a dir to store database files (default: user home dir)
-const string base_dir = getenv("HOME");
+const auto video_dirs    = std::to_array<const string>( VIDEO_DIRS );
+const auto video_dbfiles = std::to_array<const string>( VIDEO_DBS  );
 
-const auto video_dirs = std::to_array<const string>(
- {
-  "/media/chris/xeon_movies1",
-  "/media/chris/xeon_movies2",
-  "/media/chris/xeon_movies3",
-  "/media/chris/TV",
-  "/media/chris/video_460G"
-  } );
-  
-const auto video_dbfiles = std::to_array<const string>( 
-  { base_dir + "/" + ".mediadb.xeon_movies1",
-    base_dir + "/" + ".mediadb.xeon_movies2",
-    base_dir + "/" + ".mediadb.xeon_movies3",
-    base_dir + "/" + ".mediadb.tv"          ,
-    base_dir + "/" + ".mediadb.video_460G"
-  } );
+const auto audio_dirs    = std::to_array<const string>( AUDIO_DIRS );  
+const auto audio_dbfiles = std::to_array<const string>( AUDIO_DBS  ); 
 
-const auto audio_dirs = std::to_array<const string>(
-  {
-    "/Audio",
-    "/Audio2",
-    "/Audio3",
-    "/Audio4",
-   } );
-  
-const auto audio_dbfiles = std::to_array<const string>( 
-  { base_dir + "/" + ".mediadb.Audio",
-    base_dir + "/" + ".mediadb.Audio2",
-    base_dir + "/" + ".mediadb.Audio3",
-    base_dir + "/" + ".mediadb.Audio4"
-  } );
-
-
+// TODO
 #ifdef USE_PICTURES
  const auto pic_dirs = std::to_array<const string>(
    {
-     base_dir + "/" + "Pictures";
+     BASE("Pictures")
    } );
 
  const auto pic_dbfiles = std::to_array<const string>(
    {
-     base_dir + "/" + ".mediadb.Pictures"
+     BASE(".mediadb.Pictures")
    } );
+
 #endif
+
 
 
 // lamdba makes correlation
